d4a2a6a4e93f18b69b3b9e0f87951feaa952bbc8
mogfull7777 <anjunyoung94@gmail.com>
2023-04-27 07:59:01 +0900

    shopping_app #2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import router from "./router";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 
 const App = () => {
     return (
         <div>
-          
+            <Header />
+            <RouterProvider router={router} />
+            <Footer />
         </div>
     );
 };
---
+++ src/pages/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>Im main</h1>
+        </div>
+    );
+};
+
+export default Main;
